587f94cad56125d1186a46f58337802ce62f633a
NAndroidEx <dev.nae.me@gmail.com>
2020-04-11 18:23:58 +0530

    Initial Commit

---
+++ app/src/main/java/com/nandroidex/backgroundservice/MainActivity.java
@@ -0,0 +1,44 @@
+package com.nandroidex.backgroundservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Intent serviceIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BackgroundService backgroundService = new BackgroundService();
+        serviceIntent = new Intent(MainActivity.this, backgroundService.getClass());
+        if (!isMyServiceRunning(backgroundService.getClass())) {
+            startService(serviceIntent);
+        }
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                Log.i("isMyServiceRunning?", true + "");
+                return true;
+            }
+        }
+        Log.i("isMyServiceRunning?", false + "");
+        return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(serviceIntent);
+        super.onDestroy();
+    }
+}
