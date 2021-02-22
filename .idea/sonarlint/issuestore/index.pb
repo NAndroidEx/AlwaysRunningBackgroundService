ef0013772fb544214ad32e9f108074ce3e7a468e
Niraj Prajapati <niraj06081995@gmail.com>
2021-02-22 19:38:01 +0530

    updated latest code

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+t
+Dapp/src/main/java/com/nandroidex/backgroundservice/MainActivity.java,6\f\6ff6f4844fcc38c8da7092e97f8c7530abf74737
+€
+Papp/src/main/java/com/nandroidex/backgroundservice/RestartBroadcastReceiver.java,2\8\285d1b044c27547f2c806d6f07e6c94af15a4d9a
+y
+Iapp/src/main/java/com/nandroidex/backgroundservice/BackgroundService.java,f\9\f9305e8a19d14e792dd426f73f2cc5c86c4cf799
--- app/src/main/java/com/nandroidex/backgroundservice/BackgroundService.java
+++ app/src/main/java/com/nandroidex/backgroundservice/BackgroundService.java
@@ -3,13 +3,18 @@ package com.nandroidex.backgroundservice;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.RingtoneManager;
 import android.os.Build;
+import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
@@ -20,10 +25,14 @@ import java.util.TimerTask;
 
 public class BackgroundService extends Service {
     public int counter = 0;
+    boolean restart = true;
+    private RemoteViews notificationLayout;
+    private Notification notification;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        notificationLayout = new RemoteViews(getPackageName(), R.layout.layout_service_notification);
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O)
             startMyOwnForeground();
         else
@@ -34,7 +43,7 @@ public class BackgroundService extends Service {
     private void startMyOwnForeground() {
         String NOTIFICATION_CHANNEL_ID = "service";
         String channelName = "Background Service";
-        NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+        NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_MIN);
         chan.setLightColor(Color.BLUE);
         chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
 
@@ -43,12 +52,21 @@ public class BackgroundService extends Service {
         manager.createNotificationChannel(chan);
 
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
-        Notification notification = notificationBuilder.setOngoing(true)
-                .setContentTitle("App is running in background")
+        notification = notificationBuilder.setOngoing(true)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContent(notificationLayout)
                 .setPriority(Notification.PRIORITY_MIN)
                 .setCategory(Notification.CATEGORY_SERVICE)
+                .setContentIntent(
+                        PendingIntent.getActivity(
+                                this, 0,
+                                new Intent(BackgroundService.this, MainActivity.class),
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        )
+                )
                 .build();
         startForeground(2, notification);
+        restart = true;
     }
 
 
@@ -65,10 +83,12 @@ public class BackgroundService extends Service {
         super.onDestroy();
         stoptimertask();
 
-        Intent broadcastIntent = new Intent();
-        broadcastIntent.setAction("restartservice");
-        broadcastIntent.setClass(this, RestartBroadcastReceiver.class);
-        this.sendBroadcast(broadcastIntent);
+        if (restart) {
+            Intent broadcastIntent = new Intent();
+            broadcastIntent.setAction("restartservice");
+            broadcastIntent.setClass(this, RestartBroadcastReceiver.class);
+            this.sendBroadcast(broadcastIntent);
+        }
     }
 
 
@@ -76,13 +96,64 @@ public class BackgroundService extends Service {
     private TimerTask timerTask;
 
     public void startTimer() {
-        timer = new Timer();
-        timerTask = new TimerTask() {
-            public void run() {
-                Log.i("Count", "=========  " + (counter++));
+//        timer = new Timer();
+//        timerTask = new TimerTask() {
+//            public void run() {
+//                Log.i("Count", "=========  " + (counter++));
+//            }
+//        };
+//        timer.schedule(timerTask, 1000, 1000); //
+        new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.i("Count", "=========  " + (millisUntilFinished / 1000));
+                if (notification != null) {
+                    notificationLayout.setTextViewText(R.id.tvDetail, "Task will be done in " + (millisUntilFinished / 1000) + " second(s)");
+                    NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    mNotificationManager.notify(2, notification);
+                }
             }
-        };
-        timer.schedule(timerTask, 1000, 1000); //
+
+            @Override
+            public void onFinish() {
+                showNotification();
+            }
+        }.start();
+    }
+
+    private void showNotification() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel("timer_channel", "Timer Channel", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Channel Description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        notificationLayout.setTextViewText(R.id.tvDetail, "10 Seconds Completed");
+
+        Notification builder = new NotificationCompat.Builder(this, "timer_channel")
+                .setVibrate(new long[]{0, 1000, 500, 1000})
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContent(notificationLayout)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText("10 Seconds Completed")
+                .setAutoCancel(true)
+                .setContentIntent(
+                        PendingIntent.getActivity(
+                                this, 0,
+                                new Intent(BackgroundService.this, MainActivity.class),
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        )
+                )
+                .build();
+        notificationManager.notify(1, builder);
+
+        restart = false;
+        stopSelf();
     }
 
     public void stoptimertask() {
